0dfbc1f18b80ac905f5fcef59b82d6d8adf39d68
Quintin Mason <122917536+Qballzzz@users.noreply.github.com>
2023-03-25 13:01:43 -0600

    Update Queue.java

--- Main/Java/Type/Queue.java
+++ Main/Java/Type/Queue.java
@@ -1,6 +1,6 @@
 package queue;
 
-import java.util.Iterator;
+import utilities.Iterator;
 import java.util.NoSuchElementException;
 
 import ADTs.QueueADT;
@@ -14,16 +14,12 @@ public class MyQueue<E> implements QueueADT{
 	int size;
 	Node head;
 	Node tail;
-	Node next;
-	Node previous;
 	DoublyLinkedList dll;
 	
 	public MyQueue(int s) {
 		this.size = s;
 		this.head = null;
 		this.tail = null;
-		this.next = null;
-		this.previous = null;
 		this.dll = new DoublyLinkedList();
 	}
 
@@ -35,13 +31,8 @@ public class MyQueue<E> implements QueueADT{
 		} else {
 			if (this.isFull()) {
 				System.out.println("Queue is full");
-			} else if (this.isEmpty()) {
-				this.dll.insertAtEnd(toAdd);
-				this.tail.data = toAdd;
-				this.head.data = toAdd;
 			} else {
 				this.dll.insertAtEnd(toAdd);
-				this.tail.data = toAdd;
 			}
 		}
 	}
@@ -57,7 +48,11 @@ public class MyQueue<E> implements QueueADT{
 
 	@Override
 	public Object peek() throws EmptyQueueException {
-		return this.head.data;
+		if (this.isEmpty()) {
+			throw new EmptyQueueException();
+		} else {
+			return this.dll.head.data;
+		}
 	}
 
 	@Override
@@ -79,58 +74,32 @@ public class MyQueue<E> implements QueueADT{
 	public Iterator iterator() {
 		return new QueueIterator();
 	}
-	private class QueueIterator implements Iterator<E> {
-        @SuppressWarnings("unchecked")
-		private Node<E> current = head;
-        private Node<E> last = null;
-
+	private class QueueIterator implements Iterator {
+        private Node current = head;
         @Override
         public boolean hasNext() {
-            return current != null;
+            return current.next != null;
         }
+        @SuppressWarnings("unchecked")
         @Override
         public E next() {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
-            E data = current.data;
-            last = current;
+			E data = (E) current.data;
             current = current.next;
             return data;
         }
-
-        @SuppressWarnings("unchecked")
-		@Override
-        public void remove() {
-            if (last == null) {
-                throw new IllegalStateException();
-            }
-            if (last.previous == null) {
-                head = last.next;
-                if (head != null) {
-                    head.previous = null;
-                } else {
-                    tail = null;
-                }
-            } else if (last.next == null) {
-                tail = last.previous;
-                tail.next = null;
-            } else {
-                last.previous.next = last.next;
-                last.next.previous = last.previous;
-            }
-            size--;
-            last = null;
-        }
 	}
-
 	@Override
 	public boolean equals(QueueADT that) {
-		if (this.iterator() == that.iterator()) {
-			return true;
-		} else {
-			return false;
+		while (this.iterator().hasNext()) {
+			if (this.iterator().next().equals(that.iterator().next())) {
+			} else {
+				return false;
+			}
 		}
+		return true;
 	}
 
 	@Override
@@ -162,3 +131,4 @@ public class MyQueue<E> implements QueueADT{
 	}
 	
 }
+
